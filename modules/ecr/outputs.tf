63c72c34cb8b84bf43d00dcd1e3cf580f70610a7
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-28 00:20:20 +0500

    Update outputs.tf

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,23 +1,5 @@
 
-output "repository_arn" {
-  description = "Full ARN of the repository"
-  value       = aws_ecr_repository.main.arn
-}
-
-output "repository_name" {
-  description = "Name of the repository"
-  value       = aws_ecr_repository.main.name
-}
-
-output "repository_url" {
-  description = "URL of the repository"
-  value       = aws_ecr_repository.main.repository_url
-}
-
-output "registry_id" {
-  description = "Registry ID where the repository was created"
-  value       = aws_ecr_repository.main.registry_id
-}
+# IAM Role outputs for ECS module integration (ECR repository outputs are already defined in main.tf)
 
 output "ecs_task_execution_role_arn" {
   description = "ARN of the ECS task execution role"
