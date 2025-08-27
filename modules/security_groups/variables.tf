7585f2a67ebf3b1871c70ab6e527b9acef9d742d
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-27 23:53:52 +0500

    Create variables.tf

---
+++ modules/security_groups/variables.tf
@@ -0,0 +1,16 @@
+
+variable "name_prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the security group"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev-sandbox)"
+  type        = string
+  default     = "dev-sandbox"
+}
