cddddee59b94bf0b43665b51e0ca30eac10eb4c6
AbdulRehman-Devops150 <abdulrehman.rbbn177@gmail.com>
2025-08-28 15:57:04 +0500

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_internet_gateway" "main" {
 
 # Elastic IPs for NAT Gateways
 resource "aws_eip" "nat" {
-  count  = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
+  count  = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.public_subnet_cidrs)) : 0
   domain = "vpc"
 
   depends_on = [aws_internet_gateway.main]
@@ -67,7 +67,7 @@ resource "aws_subnet" "private" {
 
 # NAT Gateways
 resource "aws_nat_gateway" "main" {
-  count = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
+  count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.public_subnet_cidrs)) : 0
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
@@ -103,7 +103,7 @@ resource "aws_route_table" "private" {
     for_each = var.enable_nat_gateway ? [1] : []
     content {
       cidr_block     = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.main[count.index].id
+      nat_gateway_id = var.single_nat_gateway ? aws_nat_gateway.main[0].id : aws_nat_gateway.main[count.index].id
     }
   }
 
